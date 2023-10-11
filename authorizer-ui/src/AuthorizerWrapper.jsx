69cd950ed40fdff57d353b44156edd2ece742a35
oleg1322 <oo1322765@gmail.com>
2023-10-12 01:24:16 +0300

    authorizer_login_page_nofunc

---
+++ authorizer-ui/src/AuthorizerWrapper.jsx
@@ -0,0 +1,21 @@
+import style from './AuthorizerWrapper.module.css'
+
+export const AuthorizerWrapper = () => {
+  return (
+    <div className={style.wrapper}>
+        <div className={style.container}>
+            <div className={style.logo}>
+                <img src="/logo.svg" alt="logo" />
+            </div>
+            <div>
+                <input className={style.input} placeholder='Телефон / email'/>
+            </div>
+            <div>
+                <input className={style.input} placeholder='Пароль'/>
+            </div>
+            <div className={style.restore}>Восстановить пароль</div>
+            <div className={style.button}>Войти</div>
+        </div>
+    </div>
+  )
+}
--- authorizer-ui/src/main.jsx
+++ authorizer-ui/src/main.jsx
@@ -1,10 +1,10 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
 import './index.css'
+import { AuthorizerWrapper } from './AuthorizerWrapper.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <AuthorizerWrapper />
   </React.StrictMode>,
 )
